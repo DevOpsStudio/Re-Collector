b00e8e7d0e4fdf8768b46000fefb5be60dd26c97
kira <529280602@qq.com>
2017-07-30 00:14:59 +0800

    升级Guava为22.0,优化MessageFields,使用Java8特性

--- src/main/java/org/graylog/collector/CollectorVersion.java
+++ src/main/java/org/graylog/collector/CollectorVersion.java
@@ -1,16 +1,16 @@
 /**
  * This file is part of Graylog.
- *
+ * <p>
  * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -19,11 +19,13 @@ package org.graylog.collector;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 import static com.google.common.base.Strings.isNullOrEmpty;
 
 public class CollectorVersion {
     public static final CollectorVersion CURRENT;
+    static Logger LOG = Logger.getLogger(CollectorVersion.class.getName());
 
     static {
         String version = "NONE";
@@ -45,6 +47,7 @@ public class CollectorVersion {
                 commitIdShort = commitId.substring(0, 7);
             }
         } catch (IOException ignored) {
+            LOG.info("Can not Load The Properties");
         }
 
         CURRENT = new CollectorVersion(version, commitId, commitIdShort, timestamp);
@@ -80,6 +83,6 @@ public class CollectorVersion {
 
     @Override
     public String toString() {
-        return "v" + version() + "(commit " + commitIdShort() + ")";
+        return String.format("v%s(commit%s)", version(), commitIdShort());
     }
 }
--- src/main/java/org/graylog/collector/MessageFields.java
+++ src/main/java/org/graylog/collector/MessageFields.java
@@ -1,16 +1,16 @@
 /**
  * This file is part of Graylog.
- *
+ * <p>
  * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -61,10 +61,9 @@ public class MessageFields {
     @Override
     public String toString() {
         final MoreObjects.ToStringHelper stringHelper = MoreObjects.toStringHelper(this);
-
-        for (Map.Entry<String, Object> entry : asMap().entrySet()) {
-            stringHelper.add(entry.getKey(), entry.getValue());
-        }
+        asMap().forEach((k, v) -> {
+            stringHelper.add(k, v);
+        });
 
         return stringHelper.toString();
     }
